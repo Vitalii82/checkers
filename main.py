7846584971bf16badfdc7c8ae79b37adff912b23
Vitalii82 <svinovej90@gmail.com>
2021-11-18 06:15:40 +0100

    Finished vithout mini-max

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
 import pygame
-from checkers import board
-from checkers.constants import SQUARE_SIZE, WIDTH, HEIGHT
-from checkers.board import board
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.game import Game
 
 FPS = 60
 
-WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
 def get_row_col_from_mouse(pos):
-    x,y = pos
+    x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
@@ -17,26 +17,26 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
-    Board = board()
-    
-
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
-        
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get.pos()
-            row, col = get_row_col_from_mois==use(pos)
-            piece = board.get.piese(row, col)
-            board.move(piece, (4, 3)
-        
-    board.pygame.sprite.draw(surface, bgd=None)(WIN)
-    pygame.display.update()
-                
-pygame.quit()
+        game.update()
+    
+    pygame.quit()
 
 main()
